685058847f6ea7cfcb8f58afa919db2cee5e6a2e
Dhawgupta <dhawal.gupta.iitp@gmail.com>
2017-07-10 06:20:49 +0000

    Initial Commit

---
+++ car_lib.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import time
+import serial
+class car:
+    def __init__(self, port = '/dev/ttyACM0',baud_rate = 9600):
+        try:
+            self.car = serial.Serial(port, 9600)
+            print "Arduino connected on port {}".format(port)
+        except (RuntimeError, TypeError, NameError):
+            print "Not able to connect arduino on port {}".format(port)
+        self.Lval_for_motor = 0; # this is the lower value of PWM for motor
+        self.Hval_for_motor = 255;
+        self.lastThrottle = 0
+    #entering the analog value for left adn right wheel between 0 - 100
+    def map(self, val,inputL, inputH, outputL , outputH ):
+        val1 = (float(val)/(inputH - inputL))*(outputH - outputL ) + outputL
+        return int(val1)
+    def forward(self,aleft, aright):
+        aleft = self.map(aleft, 0, 100, self.Lval_for_motor, self.Hval_for_motor )
+        aright = self.map(aright,0, 100, self.Lval_for_motor, self.Hval_for_motor )
+        self.car.write('w' + chr(aleft) + chr(aright))
+
+    def right(self,aleft, aright):
+        aleft = self.map(aleft,0, 100, self.Lval_for_motor, self.Hval_for_motor )
+        aright = self.map(aright,0, 100, self.Lval_for_motor, self.Hval_for_motor )
+        self.car.write('d' + chr(aleft) + chr(aright))
+
+    def left(self,aleft, aright):
+        aleft = self.map(aleft,0, 100, self.Lval_for_motor, self.Hval_for_motor )
+        aright = self.map(aright,0, 100, self.Lval_for_motor, self.Hval_for_motor )
+        self.car.write('a' + chr(aleft) + chr(aright))
+
+    def reverse(self,aleft, aright):
+        aleft = self.map(aleft,0, 100, self.Lval_for_motor, self.Hval_for_motor )
+        aright = self.map(aright,0, 100, self.Lval_for_motor, self.Hval_for_motor )
+        self.car.write('s' + chr(aleft) + chr(aright))
+
+    def stop(self,aleft = 0, aright = 0):
+        aleft = self.map(aleft,0, 100, self.Lval_for_motor, self.Hval_for_motor )
+        aright = self.map(aright,0, 100, self.Lval_for_motor, self.Hval_for_motor )
+        self.car.write('x' + chr(aleft) + chr(aright))
+
+    def throttle(self, throt):
+        self.forward(throt, throt)
+        self.lastThrottle
+    def brake(self, mode = 0 , brak = 100):
+        # crearing different kind of modes
+        '''
+        mode = 0 => taking the forward pwm to 0
+        mode = 1 => enabling the halt feature in motor driver"
+        mode = 2 => delayed halt while taking pwm down slowly
+        '''
+        if mode == 0:
+            self.forward(0,0)
+        elif mode == 1:
+            self.stop()
+        elif mode == 2:
+            while val > 0:
+                self.forward(self.lastThrottle, self.lastThrottle)
+                self.lastThrottle = self.lastThrottle - brak
+                # tune the sleep value
+                time.sleep(0.01)
+
---
+++ keyboard_control.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+from car_lib import *
+import usb.core
+import usb.util
+import serial
+import time
+
+dagu = car()
+
+
+USB_VENDOR  = 0x3938 # Rii
+USB_PRODUCT = 0x1032 # Mini Wireless Keyboard
+
+USB_IF      = 0 # Interface
+USB_TIMEOUT = 5 # Timeout in MS
+
+BTN_LEFT  = 80
+BTN_RIGHT = 79
+BTN_DOWN  = 81
+BTN_UP    = 82
+BTN_STOP  = 44 # Space
+BTN_EXIT  = 41 # ESC
+
+dev = usb.core.find(idVendor=USB_VENDOR, idProduct=USB_PRODUCT)
+endpoint = dev[0][(0,0)][0]
+
+if dev.is_kernel_driver_active(USB_IF) is True:
+  dev.detach_kernel_driver(USB_IF)
+
+usb.util.claim_interface(dev, USB_IF)
+
+
+
+while True:
+    control = None
+    try:
+        control = dev.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize, USB_TIMEOUT)
+        print(control)
+    except:
+        pass
+
+    if control != None:
+        if BTN_DOWN in control:
+            dagu.reverse()
+            print "Going back"
+
+        if BTN_UP in control:
+            dagu.throttle(75)
+	   
+            print "Going Forward"
+
+        if BTN_LEFT in control:
+            dagu.left(40, 90)
+	   
+	    print "Going left"
+
+
+        if BTN_RIGHT in control:
+	    dagu.right(90, 40)
+	    dagu.stop()
+            print "Going Right"
+
+        if BTN_STOP in control:
+            dagu.brake()
+            print "Stopped"
+
+        if BTN_EXIT in control:
+            exit()
+
